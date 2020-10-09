aef36c6f46bc981d1481311a44654f19b7737548
Hsing-Hui Hsu <hsinghui@amazon.com>
2020-10-09 00:12:12 -0700

    add stacks

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkpipelinesDemoStack } from '../lib/cdkpipelines-demo-stack';
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
 
-const app = new cdk.App();
-new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack');
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '794715269151', region: 'us-east-2' },
+});
+
+app.synth();
